4ad4f7e0a2ff493dfe7c30750f389875459e55a3
David Loveday <davemech@yahoo.com>
2018-01-26 13:05:15 -0800

    incomplete work on Carousel.js and index.html

---
+++ ANSWERS.md
@@ -0,0 +1,3 @@
+1. The DOM is an interface that allows a programming language to manipulate the content, structure, and style of a website.
+2. An event is an action that occurs as a result of the user or another source, such as a mouse being clicked, or a key being pressed. 
+3. An event listener is a procedure or function in a computer program that waits for an event to occur.
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,5 +1,35 @@
 class Carousel {
-
+    constructor(containererID) {
+        this.container = document.getElementById(containerID) || document.body;
+        this.slides = this.container.querySelectorAll(".Carousel");
+        this.total = this.slides.length - 1;
+        this.current = 0;
+        this.slide(this.current);
+    }
+    next(interval) {
+        (this.current === this.total) ? this.current = 0 : this.current += 1;
+        this.stop();
+        this.slide(this.current);
+        if (typeof interval === 'number' && (interval % 1) === 0) {
+            let context = this;
+            this.run = setTimeout(() => {
+            context.next(interval);    
+            }, interval);
+        }
+    }
+    stop() {
+        clearTimeout(this.run);
+    }
+    slide(index) {
+        if (index >= 0 && index <= this.total) {
+            this.stop();
+            for (let i = 0; i <= this.total; i++) {
+                if (i === index) {
+                    this.slides[i];
+                } 
+            }
+        } 
+    }
 }
 
 let carousels = document.querySelectorAll(".Carousel");
